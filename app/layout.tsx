d3394d7faede63d7636776ce10b9707aaa257338
Teagueporter <37876539+Teagueporter@users.noreply.github.com>
2024-05-04 13:11:37 -0600

    updating layouts

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,34 +1,38 @@
-import type { Metadata } from "next";
-import { IBM_Plex_Sans } from "next/font/google";
-import "./globals.css";
-import { cn } from "../lib/utils";
-import { ClerkProvider, SignInButton, SignedIn, SignedOut, UserButton } from '@clerk/nextjs'
+import type { Metadata } from 'next'
+import { IBM_Plex_Sans } from 'next/font/google'
+import './globals.css'
+import { cn } from '../lib/utils'
+import {
+  ClerkProvider,
+  SignInButton,
+  SignedIn,
+  SignedOut,
+  UserButton,
+} from '@clerk/nextjs'
 
-
-const IBMPlex = IBM_Plex_Sans({ subsets: ["latin"],
+const IBMPlex = IBM_Plex_Sans({
+  subsets: ['latin'],
   weight: ['400', '500', '600', '700'],
-  variable: '--font-ibm-plex' 
- });
+  variable: '--font-ibm-plex',
+})
 
 export const metadata: Metadata = {
-  title: "Imaginify",
-  description: "AI-powered image generator for the web",
-};
+  title: 'Imaginify',
+  description: 'AI-powered image generator for the web',
+}
 
 export default function RootLayout({
   children,
 }: Readonly<{
-  children: React.ReactNode;
+  children: React.ReactNode
 }>) {
   return (
-    <ClerkProvider appearance={{variables: { colorPrimary: '#624cf5'}}}>
-      <html lang="en">
-        <body className={cn("font-IBMPlex antialiased", IBMPlex.variable)}>
-          <main>
-            {children}
-          </main>
+    <ClerkProvider appearance={{ variables: { colorPrimary: '#624cf5' } }}>
+      <html lang='en'>
+        <body className={cn('font-IBMPlex antialiased', IBMPlex.variable)}>
+          <main>{children}</main>
         </body>
       </html>
     </ClerkProvider>
-  );
+  )
 }
