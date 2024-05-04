03a9c4bd247310afbd4e3abf8fedfe97c065a389
Teagueporter <37876539+Teagueporter@users.noreply.github.com>
2024-05-04 13:12:13 -0600

    image api

--- app/(root)/transformations/add/[type]/page.tsx
+++ app/(root)/transformations/add/[type]/page.tsx
@@ -1,8 +1,33 @@
 import React from 'react'
+import Header from '../../../../../components/shared/Header'
+import TranformationForm from '../../../../../components/shared/TransformationForm'
+import { SearchParamProps, TransformationTypeKey } from '../../../../../types'
+import { transformationTypes } from '../../../../../constants'
+import { auth } from '@clerk/nextjs/server'
+import { getUserById } from '../../../../../lib/actions/user.actions'
+import { redirect } from 'next/navigation'
 
-const AddTransormationTypePage = () => {
+const AddTransormationTypePage = async ({
+  params: { type },
+}: SearchParamProps) => {
+  const transformation = transformationTypes[type]
+  const { userId } = auth()
+
+  if (!userId) redirect('/sign-in')
+
+  const user = await getUserById(userId)
   return (
-    <div>AddTransormationTypePage</div>
+    <>
+      <Header title={transformation.title} subtitle={transformation.subTitle} />
+      <section className='mt-10'>
+        <TranformationForm
+          action='Add'
+          userId={user._id}
+          type={transformation.type as TransformationTypeKey}
+          creditBalance={user.creditBalance}
+        />
+      </section>
+    </>
   )
 }
 
